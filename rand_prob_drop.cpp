c5aaf2d43c5afbee7867975de8bbe60f11d3ac80
Akshay-Ravi <akshaynitt97@gmail.com>
2022-04-03 09:21:12 -0700

    Update rand_prob_drop.cpp

--- rand_prob_drop.cpp
+++ rand_prob_drop.cpp
@@ -4,14 +4,14 @@ using namespace std;
 int main()
 {
     vector<int>dropped,seq;
-    int window_size=150,last_packet_number=3;
-    int number_of_dropped=window_size*0.01;
+    int no_of_packets=150,first_packet_number=3;
+    int number_of_dropped=no_of_packets*0.01;
     bool flag=false;
     if(number_of_dropped>=1)
     {
         for(int i=0;i<number_of_dropped;i++)
-            dropped.push_back(rand()%(window_size+1)+last_packet_number);
-        for(int i=last_packet_number;i<last_packet_number+1+window_size;i++)
+            dropped.push_back(rand()%(no_of_packets+1)+first_packet_number);
+        for(int i=first_packet_number;i<first_packet_number+1+no_of_packets;i++)
         {
             flag=false;
             for(int j=0;j<dropped.size();j++)
@@ -28,7 +28,7 @@ int main()
     }
     else
     {
-        for(int i=last_packet_number;i<=last_packet_number+1+window_size;i++)
+        for(int i=first_packet_number;i<=first_packet_number+1+no_of_packets;i++)
             seq.push_back(i);
     }
     for(int packet:dropped)
